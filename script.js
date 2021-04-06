0f451580e58e52673d825a95746e294819bda59f
Nathan_Wichmann <nathanwicker@hotmail.com>
2021-04-06 12:54:11 -0400

    updates

--- script.js
+++ script.js
@@ -8,34 +8,62 @@ var inputEll = "";
 //this creates the history array that is stored to the local storage with a key name history or array 
 var historyArr = JSON.parse(localStorage.getItem('history')) || [];
 //this sets the variables lat and lon with no value 
- var lat;
- var lon;
+var lat;
+var lon;
 
-
+document.getElementById("currentWeather").style.display = "none";
+document.getElementById("weather").style.display = "none";
 //this gets the uv index form the lat and lon coordinates api and creates the p tag to display the value and span is used b/c a p tag only takes text not number values 
 function getUvIndex() {
     fetch(`http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`)
-    .then(response => response.json())
-        .then((data) =>{
-        //    console.log('Fetch Response \n-------------');
-        //    console.log(data.value)
-           var uvEl = $('<p>').text('UV Index: ');
-           var span = $('<span>').text(data.value)
+        .then(response => response.json())
+        .then((data) => {
 
-           $('#currentWeather .currentWeather').append(uvEl.append(span));
-        })
-    }
+            var uvEl = $('<p>').text('UV Index: ');
+            var span = $('<span>').text(data.value)
+
+            $('#currentWeather .currentWeather').append(uvEl.append(span));
+
+            if (data.value > 0 && data.value <= 2) {
+                uvEl.attr('class', 'green');
+            }
+            else if (data.value > 2 && data.value <= 5) {
+                uvEl.attr('class', 'yellow')
+            }
+            else if (data.value > 5 && data.value <= 7) {
+                uvEl.attr('class', 'orange')
+            }
+            else if (data.value > 7 && data.value <= 10) {
+                uvEl.attr('class', 'red')
+            }
+            else {
+                uvEl.attr('class', 'purple')
+            }
+
+        });
+}
+
 
 function getWeatherForecast() {
     fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${cityInput}&appid=${apiKey}&units=metric`)
         .then(response => response.json())
         .then((data) => {
             // console.log('Fetch Response \n-------------');
+            $('.forecast').empty();
+            document.getElementById("weather").style.display = "block";
 
             for (let i = 0; i < data.list.length; i++) {
                 if (data.list[i].dt_txt.indexOf('15:00:00') !== -1) {
                     console.log(data.list[i])
-                
+
+                    var skyConditions = data.list[i].weather[0].icon
+
+                    console.log(data.list[i].weather[0].icon)
+
+
+
+                    var icon = 'http://openweathermap.org/img/wn/' + skyConditions + '@2x.png';
+                    console.log(icon);
                     var dataEl = data.list[i].dt_txt
 
                     var tempatureEl = data.list[i].main.temp
@@ -44,37 +72,9 @@ function getWeatherForecast() {
                     var minTemp = data.list[i].main.temp_min
                     var humidityEl = data.list[i].main.humidity
 
-//                         < div class="card" style = "width: 18rem;" >
-//                             <div class="card-body">
-//                                 <h5 class="card-title">Card title</h5>
-//                                 <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-//                                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-//                                 <a href="#" class="card-link">Card link</a>
-//                                 <a href="#" class="card-link">Another link</a>
-//                             </div>
-//                          </div >
-//                         < div class="card" style = "width: 18rem;" >
-//                             <div class="card-body">
-//                                 <h5 class="card-title">Card title</h5>
-//                                 <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-//                                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-//                                 <a href="#" class="card-link">Card link</a>
-//                                 <a href="#" class="card-link">Another link</a>
-//                             </div>
-//                          </div >
-//                         < div class="card" style = "width: 18rem;" >
-//                             <div class="card-body">
-//                                 <h5 class="card-title">Card title</h5>
-//                                 <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-//                                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-//                                 <a href="#" class="card-link">Card link</a>
-//                                 <a href="#" class="card-link">Another link</a>
-//                             </div>
-//                          </div >
-
                     // this is your column: no class attached yet
                     var col = document.createElement('div');
-
+                    var imgTag = document.createElement('img');
                     var card = document.createElement('div');
                     var cardBody = document.createElement('div');
                     var header6 = document.createElement('h6')
@@ -83,10 +83,11 @@ function getWeatherForecast() {
                     var cardtext = document.createElement('p');
                     var cardtextE = document.createElement('p');
 
-                    card.setAttribute('class', 'card w-25 p-3 text-center rounded bg-primary text-white');
+                    imgTag.setAttribute('src', icon);
+                    card.setAttribute('class', 'card text-center rounded bg-primary text-white');
                     cardBody.setAttribute('class', 'card-body');
                     col.setAttribute('class', 'col')
-                    header6.setAttribute('class', 'card-title');
+                    header6.setAttribute('class', 'card-title text-bold');
                     cardText.setAttribute('class', 'card-text');
                     cardTextEl.setAttribute('class', 'card-text');
                     cardtext.setAttribute('class', 'card-text');
@@ -98,34 +99,9 @@ function getWeatherForecast() {
                     cardText.textContent = ("Min Temp: " + minTemp);
                     cardtextE.textContent = ("Humidity: " + humidityEl);
                     card.append(cardBody);
-                    cardBody.append(header6, cardTextEl, cardtext, cardText, cardtextE);
+                    cardBody.append(header6, imgTag, cardTextEl, cardtext, cardText, cardtextE);
 
                     $('.forecast').append(card)
-
-
-
-                    // card.classList = $('list-item flex-row justify-space-between align-center')
-
-
-                    // var dateEll = $('<p>').text('Date: ' + dataEl)
-                    // // card.append(dateEll)
-
-                    // var cityName = $('<p>').text('City: ' + cityNameEl)
-                    // // card.append(cityName);
-                    // $('.forecast').append(card)
-                    
-                    // var tempEl = $('<p>').text('Temp: ' + tempatureEl)
-                    // // card.append(tempEl);
-                    // $('.forecast').append(card)
-                    
-                    // var maxTemp = $('<p>').text('Max Temp: ' + maxTempEl)
-                    // // card.append(maxTemp)
-                    // $('.forecast').append(card)
-
-                    // var humidity = $('<p>').text('Humidity: ' + humidityEl)
-                    // // card.append(humidity)
-                    // $('.forecast').append(card)
-
                 }
             }
         })
@@ -139,7 +115,8 @@ function getCurrentWeather() {
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=metric`)
         .then(response => response.json())
         .then((data) => {
-
+            $('#currentWeather').empty();
+            document.getElementById("currentWeather").style.display = "block";
             // console.log('Fetch Response \n-------------');
 
             var card = $('<div>').addClass('currentWeather');
@@ -147,6 +124,7 @@ function getCurrentWeather() {
             var dateEl = $('<p>').text('Date: ' + moment().format("M/D/YYYY"))
             card.append(dateEl)
             $('#currentWeather').append(card)
+
 
 
             var cityEl = $('<p>').text('City: ' + data.name)
@@ -176,10 +154,10 @@ function getCurrentWeather() {
             var tempMin = $('<p>').text('Temp Min: ' + data.main.temp_min);
             card.append(tempMin)
             $('#currentWeather').append(card);
-                lat = data.coord.lat;
-                lon = data.coord.lon;
-                
-                getUvIndex();
+            lat = data.coord.lat;
+            lon = data.coord.lon;
+
+            getUvIndex();
 
         });
 };
@@ -192,8 +170,8 @@ searchBtn.on('click', function () {
 
     // console.log(input.val());
     cityInput = $('#input-search').val();
-        
-        if(historyArr.indexOf(cityInput) === -1){
+
+    if (historyArr.indexOf(cityInput) === -1) {
 
         historyArr.push(cityInput);
         localStorage.setItem('history', JSON.stringify(historyArr));
@@ -204,14 +182,25 @@ searchBtn.on('click', function () {
 });
 
 
-function loadHistory(){
+function loadHistory() {
     //make list for history
 
     for (let i = 0; i < historyArr.length; i++) {
         // console.log(historyArr[i])
+        var history = historyArr[i]
+        var li = $('<li>').text(history)
+        li.attr('id', 'list');
+        li.attr('data', history);
+        li.attr('class', 'list-group')
+        console.log(history)
+        historyList.prepend(history)
+       
+        
+
         //make li tags w/ a button inside
         //apend li tag to UL tag on html
         //button when clicked should call getCurrentWeather
+
     }
 
 }
