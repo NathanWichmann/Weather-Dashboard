474cd0f44537c7118be140e124c7a454dabfa2de
Nathan Wichmann <nathanwicker@hotmail.com>
2021-04-09 16:32:05 -0400

    updates

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // this is the api key that allows acces to the api 
-var apiKey = "8fd343e8e0a625fd4fe2e99e70b356cb"
+var apiKey = "e3418ba2f79f3a5ca9fcb473c1215f42"
 //connects the dom to the html 
 var searchBtn = $('#searchBtn');
 //creates a cityInput variable to has no assigned value
@@ -65,7 +65,7 @@ function getWeatherForecast() {
 
                     var icon = 'http://openweathermap.org/img/wn/' + skyConditions + '@2x.png';
                     console.log(icon);
-                    var dataEl = data.list[i].dt_txt
+                    var dataEl = moment().format("M/D/YYYY")
 
                     var tempatureEl = data.list[i].main.temp
                     var maxTempEl = data.list[i].main.temp_max
@@ -93,11 +93,11 @@ function getWeatherForecast() {
                     cardtext.setAttribute('class', 'card-text');
                     cardtextE.setAttribute('class', 'card-text');
 
-                    header6.textContent = ("Date: " + dataEl);
-                    cardTextEl.textContent = ("Temp: " + tempatureEl);
-                    cardtext.textContent = ("Max Temp: " + maxTempEl);
-                    cardText.textContent = ("Min Temp: " + minTemp);
-                    cardtextE.textContent = ("Humidity: " + humidityEl);
+                    header6.textContent = (dataEl);
+                    cardTextEl.textContent = ("Temp: " + Math.round(tempatureEl) + '°C');
+                    cardtext.textContent = ("Max Temp: " + Math.round(maxTempEl) + '°C');
+                    cardText.textContent = ("Min Temp: " + Math.round(minTemp) + '°C');
+                    cardtextE.textContent = ("Humidity: " + humidityEl + '%');
                     card.append(cardBody);
                     cardBody.append(header6, imgTag, cardTextEl, cardtext, cardText, cardtextE);
 
@@ -121,50 +121,45 @@ function getCurrentWeather() {
 
             var card = $('<div>').addClass('currentWeather');
 
-            var dateEl = $('<p>').text('Date: ' + moment().format("M/D/YYYY"))
+            var dateEl = $('<p>').attr('id', 'date').text(moment().format("M/D/YYYY"))
             card.append(dateEl)
             $('#currentWeather').append(card)
 
-
-
-            var cityEl = $('<p>').text('City: ' + data.name)
+            var cityEl = $('<h2>').addClass('cityName').text(data.name)
             card.append(cityEl);
             $('#currentWeather').append(card)
 
-            var tempEl = $('<p>').text('Temperature: ' + data.main.temp);
+            var tempEl = $('<p>').text('Temperature: ' + Math.round(data.main.temp) + '°C');
             card.append(tempEl);
             $('#currentWeather').append(card);
 
-            var feelsLike = $('<p>').text('Feels Like: ' + data.main.feels_like);
+            var feelsLike = $('<p>').text('Feels Like: ' + Math.round(data.main.feels_like) + '°C');
             card.append(feelsLike)
             $('#currentWeather').append(card);
 
-            var wind = $('<p>').text('Wind Speed: ' + data.wind.speed);
+            var wind = $('<p>').text('Wind Speed: ' + data.wind.speed + 'kph');
             card.append(wind)
             $('#currentWeather').append(card);
 
-            var humidity = $('<p>').text('Humidity: ' + data.main.humidity);
+            var humidity = $('<p>').text('Humidity: ' + data.main.humidity + '%');
             card.append(humidity)
             $('#currentWeather').append(card);
 
-            var tempMax = $('<p>').text('Temp Max: ' + data.main.temp_max);
+            var tempMax = $('<p>').text('Max Temp: ' + Math.round(data.main.temp_max) + '°C');
             card.append(tempMax)
             $('#currentWeather').append(card);
 
-            var tempMin = $('<p>').text('Temp Min: ' + data.main.temp_min);
+            var tempMin = $('<p>').text('Min Temp: ' + Math.round(data.main.temp_min) + '°C');
             card.append(tempMin)
             $('#currentWeather').append(card);
             lat = data.coord.lat;
             lon = data.coord.lon;
 
             getUvIndex();
-            addHistoryitem(data.name);
+
 
         });
 };
-
-
-
 
 
 searchBtn.on('click', function () {
@@ -175,22 +170,19 @@ searchBtn.on('click', function () {
     if (historyArr.indexOf(cityInput) === -1) {
 
         historyArr.push(cityInput);
-        localStorage.setItem('history', JSON.stringify(historyArr));
+        localStorage.setItem('history', JSON.stringify(historyArr))
     }
 
     getCurrentWeather();
     getWeatherForecast();
 });
 
-function addHistoryitem(item) {
-    var li = $('<li>').text(item)
-    li.attr('id', 'list');
-    // li.attr('data', history)
-    li.attr('class', 'list-group-item')
-  
-    console.log(li)
-    historyList.append(li);
-    
+
+function historySelect() {
+
+    cityInput = this.value;
+    getCurrentWeather();
+    getWeatherForecast();
 
 }
 
@@ -199,25 +191,31 @@ function loadHistory() {
     //make list for history
 
     for (let i = 0; i < historyArr.length; i++) {
-        console.log(historyArr[i])
-        
+
+
         var history = historyArr[i];
-        addHistoryitem(history);
-        
-    
 
-        
-       
-        
-       
-        
 
-        //make li tags w/ a button inside
-        //apend li tag to UL tag on html
-        //button when clicked should call getCurrentWeather
+        var li = document.createElement('li')
+        var buttonTagEl = document.createElement('button');
 
+        buttonTagEl.classList = 'list-group-item align-center';
+        buttonTagEl.setAttribute('value', history);
+        buttonTagEl.innerHTML = history;
+        buttonTagEl.onclick = historySelect;
+
+        li.append(buttonTagEl);
+
+        var list = document.querySelector('.list-group');
+        list.appendChild(li);
     }
 
-}
+};
+
+
+
 
 loadHistory();
+
+
+
